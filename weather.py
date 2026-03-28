0e0f4b00ad13a2375e78d26011c5e469dfbb9b68
australopiter <164238849+P1teros@users.noreply.github.com>
2026-03-28 15:21:35 +0100

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,25 @@
+import adafruit_dht
+import time
+import board
+
+dhtDevice = adafruit_dht.DHT22(board.D4)
+
+while True:
+        try:
+                #odczyt temp i wilgotnosci
+                temp = dhtDevice.temperature
+                humidity = dhtDevice.humidity
+                print(f"Temp: {temp:.1f}C")
+                print(f"Humidity: {humidity:.1f}%")
+          
+        except RuntimeError as error:
+                print(f"Error: {error.args[0]}; retrying...")
+                time.sleep(2.0)
+                continue
+          
+        except Exception as error:
+                dhtDevice.exit()
+                raise error
+
+        time.sleep(2.0)
+
